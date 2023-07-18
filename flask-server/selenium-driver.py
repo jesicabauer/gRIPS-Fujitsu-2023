b3a75ece40eb8804f7d3b1e26710f03b5b6488ca
lily-ge <lilyge@umich.edu>
2023-07-18 15:14:59 +0900

    progress save

--- flask-server/selenium-driver.py
+++ flask-server/selenium-driver.py
@@ -11,7 +11,7 @@ import numpy as np
 # options.add_argument("--headless=new")
 # browser = webdriver.Chrome(options=options)
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+# options.add_argument('--headless')
 browser = webdriver.Chrome(options=options)
 
 # browser = webdriver.Chrome()
@@ -20,43 +20,47 @@ browser.get("https://widelearning.labs.fujitsu.com/en/trialTool/terms.html")
 browser.fullscreen_window()
 browser.execute_script("window.scrollTo(720, 840)")
 
-# time.sleep(5)
+time.sleep(3)
 
 browser.find_element(By.CLASS_NAME, "wl_check_styled").click()
 
-# time.sleep(2)
+time.sleep(2)
 browser.find_element(By.CLASS_NAME, "btn-inner").click()
 
-# time.sleep(5)
+time.sleep(3)
 
 file_upload = browser.find_element(By.CLASS_NAME, "input-btn")
-# file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/animals_train.csv")
+file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/animals_train.csv")
 # file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/election_train.csv")
 # file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/COVID_train.csv")
-file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/defect_prevention_train.csv")
+# file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/defect_prevention_train.csv")
+# file_upload.send_keys("/Users/lilyge/Downloads/gRIPS23/iris_train.csv")
 
-# time.sleep(5)
+time.sleep(3)
 
 browser.find_element(By.CLASS_NAME, "v-btn__content").click()
 
-# time.sleep(10)
+time.sleep(3)
 
 button_list = browser.find_elements(By.CLASS_NAME, "v-btn__content")
-print(button_list[len(button_list)-2]) # get the second to last button
+# print(button_list[len(button_list)-2]) # get the second to last button
 
 button_list[len(button_list)-2].click()
 time.sleep(5)
 browser.find_element(By.CLASS_NAME, "v-input__append-inner").click()
 
-# time.sleep(2)
+time.sleep(2)
 browser.find_elements(By.CLASS_NAME, "v-list-item")[-1].click()
 
-# time.sleep(10)
+time.sleep(3)
 
-print(browser.page_source)
+# print(browser.page_source)
 df_data = pd.read_html(browser.page_source)
 
-# df_data[0].to_csv("animal_step2_data.csv")
+df_data[0].to_csv("/Users/lilyge/Desktop/animal_step2_data.csv")
 # df_data[0].to_csv("elections_step2_data.csv")
 # df_data[0].to_csv("covid_step2_data.csv")
-df_data[0].to_csv("defect_prevention_train_step2_data.csv")
+# df_data[0].to_csv("defect_prevention_train_step2_data.csv")
+# df_data[0].to_csv("iris_step2_data.csv")
+
+print("step 2 data saved")
--- flask-server/server.py
+++ flask-server/server.py
@@ -7,9 +7,9 @@ import time
 import pandas as pd
 import numpy as np
 import json
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.svm import l1_min_c
-# import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import l1_min_c
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -88,8 +88,110 @@ def csv_to_json(csv_file):
     return data
 
 def step3_data():
-    list_dict = [{'Important_combo': 'Wings_Does not have ^ Breathes using lungs_Yes ^ Legs<4.5', 'Weight': 1.784704645868965}, {'Important_combo': 'Wings_Does not have ^ Spine_Has ^ Breathes using lungs_Yes', 'Weight': 2.9378455963829655}, {'Important_combo': 'Oviparous_No', 'Weight': 1.57897613658887}, {'Important_combo': 'Hair/fur_Has', 'Weight': 1.8693838614090996}, {'Important_combo': 'Wings_Does not have ^ Eats meat_Yes ^ Breathes using lungs_Yes', 'Weight': 0.5518301318605916}, {'Important_combo': 'Sized about the same as a cat?_No', 'Weight': -2.5825805246885833}, {'Important_combo': 'Hair/fur_Does not have', 'Weight': -3.7211725606849746}, {'Important_combo': 'Oviparous_Yes', 'Weight': -4.1592521866718295}]
-    json_data = json.dumps(list_dict)
+    #load in training and testing data
+    X_train=pd.read_csv("matrix_format.csv")
+    X_test=pd.read_csv("matrix_format_test.csv")
+
+    #gets lists of names of rows and columns
+    train_row_names=list(X_train.iloc[:,0])
+    test_row_names=list(X_test.iloc[:,0])
+
+
+    #get training label vector and remove unnecessary columns
+    y_train=X_train.iloc[:,-1]
+    X_train=X_train.iloc[:,1:-1] #removing animal name column and label column
+    X_test=X_test.iloc[:,1:] #removing animal name column
+
+    #list of all combinations
+    combo_names=list(X_test.columns)
+
+
+    #finding minimum acceptable C
+    min_C=l1_min_c(X_train,y_train,loss="log")
+    print(f"minimum acceptable C= {min_C}")
+
+
+    C_val=20
+    n_nonzero=101
+
+
+    while n_nonzero>100:
+        C_val=C_val*.9
+
+        #large C=denser beta, small C = sparser beta
+        model=LogisticRegression(penalty="l1",C=C_val,solver="liblinear",random_state=0) #sets random state for reproducability
+        #model=LogisticRegression(penalty="l1",C=1,solver="saga",random_state=0) #saga isn't converging with these default parameters
+        
+        #fit model and display results
+        classifier=model.fit(X_train,y_train)
+        
+        #get coefficients
+        coef=classifier.coef_[0]
+        nonzero_weight_indices=np.nonzero(coef)[0]
+        n_nonzero=len(nonzero_weight_indices)
+        #print(f"# of nonzero weights={n_nonzero} with C value {C_val}")
+
+        
+    print(f"# of nonzero weights={n_nonzero} with C value {C_val}")
+
+    #save results
+    predictions=classifier.predict(X_test)
+    probs=classifier.predict_proba(X_test)
+    intercept=classifier.intercept_
+
+    #display results
+
+    #predictions
+    for i in range(len(test_row_names)):
+        print(f"{test_row_names[i]} predicted as {predictions[i]} score={probs[i,1]}")
+        
+
+    #important combinations are their weights
+    print(f"number of nonzero weights={len(nonzero_weight_indices)}")
+    for i in nonzero_weight_indices:
+        #print(f"{combo_names[i]   }    weight: {coef[i]}")
+        print(combo_names[i])
+        print(f"weight: {coef[i]}")
+
+
+
+    print(f"intercept= {intercept}")
+
+
+    #print(f"prediction score if all important combos are zero (just intercept): {1/(1+np.exp(-intercept))}")
+
+
+
+    dictionary_list=[]
+    for i in nonzero_weight_indices:
+        dictionary={}
+        dictionary["Important_combo"]=combo_names[i]
+        dictionary["Weight"]=coef[i]
+        dictionary_list.append(dictionary)
+            
+        
+    # with open("animal_weights.txt", "w") as output:
+    #     output.write(str(dictionary_list))
+
+
+    # pred_dictionary_list=[]
+
+    # for i in range(len(test_row_names)):
+    #     dictionary={}
+    #     dictionary["Animal"]=test_row_names[i]
+    #     dictionary["Score"]=probs[i,1]
+    #     dictionary["Prediction"]=predictions[i]
+    #     pred_dictionary_list.append(dictionary)
+
+    # with open("animal_predictions.txt", "w") as output:
+    #     output.write(str(pred_dictionary_list))
+
+
+
+
+
+    # list_dict = [{'Important_combo': 'Wings_Does not have ^ Breathes using lungs_Yes ^ Legs<4.5', 'Weight': 1.784704645868965}, {'Important_combo': 'Wings_Does not have ^ Spine_Has ^ Breathes using lungs_Yes', 'Weight': 2.9378455963829655}, {'Important_combo': 'Oviparous_No', 'Weight': 1.57897613658887}, {'Important_combo': 'Hair/fur_Has', 'Weight': 1.8693838614090996}, {'Important_combo': 'Wings_Does not have ^ Eats meat_Yes ^ Breathes using lungs_Yes', 'Weight': 0.5518301318605916}, {'Important_combo': 'Sized about the same as a cat?_No', 'Weight': -2.5825805246885833}, {'Important_combo': 'Hair/fur_Does not have', 'Weight': -3.7211725606849746}, {'Important_combo': 'Oviparous_Yes', 'Weight': -4.1592521866718295}]
+    json_data = json.dumps(dictionary_list)
     return json_data
 
 def step6_data():
