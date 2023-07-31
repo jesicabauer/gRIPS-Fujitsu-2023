9c55e4ef00fb8d2437176f7f2589e39d218a3b67
TakeshiNakashima1 <takeshi.nakashima.c7@tohoku.ac.jp>
2023-07-31 14:11:48 +0900

    These codes display the names and weights of important combinations.

---
+++ flask-server/logistic_regression_model.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 11:25:44 2023
+
+@author: GRIPS
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+# setting dataset
+train_data_name = "animal_combos_train.csv";test_data_name = "animal_combos_test.csv"
+
+#input data
+train_data=pd.read_csv(train_data_name)
+test_data = pd.read_csv(test_data_name)
+#split data
+train_y=train_data.iloc[:,-1]
+train_x=train_data.iloc[:,1:-1] 
+test_x=test_data.iloc[:,1:]
+#get_combo
+combo_names=list(test_x.columns)
+#setting model
+model = LogisticRegression()
+# learning
+model.fit(train_x, train_y)
+# prediction
+y_pred = model.predict(test_x)
+
+weight = list(model.coef_[0])
+nonzero_weight_indices = np.nonzero(weight)[0]
+weight_dictionary_list=[]
+for i in nonzero_weight_indices:
+    dictionary={}
+    dictionary["Combo"]=combo_names[i]
+    dictionary["Weight"]=weight[i]
+    weight_dictionary_list.append(dictionary)
